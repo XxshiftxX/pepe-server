3406799f1a22293faeb1e2c540d4be14776daff2
XxshiftxX <contact@dev-shift.me>
2021-03-29 22:51:25 +0900

    add logger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
+import logger from './logger';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.json({ result: 'Hello World!' }));
 
-app.listen(port, () => console.log(`server listening on port ${port}`));
+app.listen(port, () => logger.info(`server listening on port ${port}`));
 
 export default app;
