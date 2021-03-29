59f5e1eb68376fb8cd8350f8208ba764ad6e0af0
XxshiftxX <contact@dev-shift.me>
2021-03-29 22:46:22 +0900

    add: simple express app

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-console.log('Hello World!');
+import * as express from 'express';
 
-export default () => 'success!';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.json({ result: 'Hello World!' }));
+
+app.listen(port, () => console.log(`server listening on port ${port}`));
+
+export default app;
