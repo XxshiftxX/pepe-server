f983a3a30a41592ef63069c1092010570cad2a09
XxshiftxX <contact@dev-shift.me>
2021-03-29 23:04:00 +0900

    add mongoose

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,7 @@ export default {
   app: {
     port: Number(process.env.APP_PORT ?? '3000'),
   },
+  mongo: {
+    url: process.env.MONGO_URL || '',
+  },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as mongoose from 'mongoose';
 import config from './config';
 
 import logger from './logger';
@@ -7,6 +8,8 @@ const app = express();
 const { port } = config.app;
 
 app.get('/', (req, res) => res.json({ result: 'Hello World!' }));
+
+mongoose.connect(config.mongo.url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(port, () => logger.info(`server listening on port ${port}`));
 
