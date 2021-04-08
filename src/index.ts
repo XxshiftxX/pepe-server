c1da34e6162cc364e2b93b233c236f7e68c56f06
XxshiftxX <contact@dev-shift.me>
2021-04-08 19:13:06 +0900

    change mongoose to typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import 'reflect-metadata';
+
 import * as express from 'express';
-import * as mongoose from 'mongoose';
 import config from './config';
 
 import logger from './logger';
@@ -13,8 +14,6 @@ app.use(express.json());
 app.use('/tests', testRouter);
 
 app.get('/', (req, res) => res.json({ result: 'Hello World!' }));
-
-mongoose.connect(config.mongo.url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(port, () => logger.info(`server listening on port ${port}`));
 
